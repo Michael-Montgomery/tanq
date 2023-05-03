4f1f73f92e3fff20930a54ae9030ee751cd00308
Michael <michael@Michaels-MBP.lan>
2023-05-03 16:46:23 -0400

    structure

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+    document.getElementById('welcome-image').style.opacity = 1;
+}, 100);
+
+setTimeout(function() {
+    document.getElementById('down-arrow').style.opacity = 1
+}, 5000);
